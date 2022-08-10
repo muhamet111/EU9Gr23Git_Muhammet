23c4ca3352eeed0dc27f02e097a3a06203378d33
muhamet111 <muhamet4460@gmail.com>
2022-08-10 20:57:33 +0100

    Second commit from Muhammet

--- src/muhamet.java
+++ src/muhamet.java
@@ -2,6 +2,6 @@ public class muhamet {
     public static void main(String[] args) {
 
         System.out.println("First change from Muhammet");
-        System.out.println("Second change from Muhammet");
+        System.out.println("First change from Muhammet");
     }
 }
