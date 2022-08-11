2d800749cd90dba6980f578851566ef9a52d3668
muhamet111 <muhamet4460@gmail.com>
2022-08-11 21:58:44 +0100

    new Created test 001

--- src/test.java
+++ src/test.java
@@ -1,5 +1,9 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("TEST 001");
+        System.out.println("TEST 001");
+        System.out.println("TEST 001");
+
+
     }
 }
